caf858f9ae98631d9aa00daf55e782fc8639ea73
Mikael Mellgren <mikael@murf.se>
2015-01-26 20:40:32 +0100

    user_config.h

--- include/user_config.h
+++ include/user_config.h
@@ -7,22 +7,22 @@
 
 /*DEFAULT CONFIGURATIONS*/
 
-#define MQTT_HOST			"192.168.11.122" //or "mqtt.yourdomain.com"
-#define MQTT_PORT			1880
+#define MQTT_HOST			"192.168.0.117" //or "mqtt.yourdomain.com"
+#define MQTT_PORT			1883
 #define MQTT_BUF_SIZE		1024
 #define MQTT_KEEPALIVE		120	 /*second*/
 
 #define MQTT_CLIENT_ID		"DVES_%08X"
-#define MQTT_USER			"DVES_USER"
-#define MQTT_PASS			"DVES_PASS"
+#define MQTT_ROOT_TOPIC     "iot/power/"
+#define MQTT_USER			"mqtt"
+#define MQTT_PASS			"LongTime"
 
-#define STA_SSID "DVES_HOME"
-#define STA_PASS "yourpassword"
+#define STA_SSID "Murf_2"
+#define STA_PASS "jagharenlitenkatt"
 #define STA_TYPE AUTH_WPA2_PSK
 
 #define MQTT_RECONNECT_TIMEOUT 	5	/*second*/
 
-#define DEFAULT_SECURITY	0
+#define DEFAULT_SECURITY	0 /*1=SSL 0=None*/
 #define QUEUE_BUFFER_SIZE		 		2048
-
 #endif
